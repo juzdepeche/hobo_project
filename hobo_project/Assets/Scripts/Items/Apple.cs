2bed522bdb7bf5d3c62e18449769fec94d6f8b60
Justin Gagnon-Lavoie <juzdepeche@gmail.com>
2019-10-04 23:31:58 -0400

    Add apples to player's inventory

---
+++ hobo_project/Assets/Scripts/Items/Apple.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public float lifeTime;
+    public float value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyApple", lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyApple()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag.Contains("Player") && other.collider.GetComponent<Inventory>())
+        {
+            other.gameObject.GetComponent<Inventory>().AddApple(value);
+            DestroyApple();
+        }
+    }
+}
