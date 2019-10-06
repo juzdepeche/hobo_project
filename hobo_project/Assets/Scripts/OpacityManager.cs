61f3bb0ab85020f33a0ca0a0f77dad3b5410f548
Raphael Fortin <raphael@bamboosoft.ca>
2019-10-05 23:17:48 -0400

    v1

---
+++ hobo_project/Assets/Scripts/OpacityManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpacityManager : MonoBehaviour
+{
+    public GameObject building;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {            
+            var mesh = building.GetComponent<MeshRenderer>();
+            if (mesh)
+                mesh.material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f); ;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var mesh = building.GetComponent<MeshRenderer>();
+            if (mesh)
+                mesh.material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+    }
+}
