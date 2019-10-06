0620f84f047739d1480b8d36bb7c131e7caf73bc
Justin Gagnon-Lavoie <juzdepeche@gmail.com>
2019-10-06 12:59:27 -0400

    New textures

---
+++ hobo_project/Assets/Scripts/Items/Tent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tent : MonoBehaviour
+{
+    public Material Skin1;
+    public Material Skin2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = Random.Range(0, 2) == 0 ? Skin1 : Skin2;
+    }
+}
