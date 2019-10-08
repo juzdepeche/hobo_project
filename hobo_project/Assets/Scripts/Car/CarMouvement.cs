5342eb8b13934fd8d89ffa2ff0a22c5eaf8d96a7
Justin Gagnon-Lavoie <juzdepeche@gmail.com>
2019-10-08 19:24:03 -0400

    Modify car hit boxes

--- hobo_project/Assets/Scripts/Car/CarMouvement.cs
+++ hobo_project/Assets/Scripts/Car/CarMouvement.cs
@@ -39,32 +39,4 @@ public class CarMouvement : MonoBehaviour
             GetComponent<Collider>().isTrigger = false;
         }
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player" && !hasToStop)
-        {
-            var player = other.gameObject.GetComponent<PlayerController>();
-            GameController.Instance.NotifyPlayerState(player, "player_death", true, KillPlayer);
-        }
-    }
-
-    private IStateResponse KillPlayer(GameObject player)
-    {
-        BaseResponse response = new BaseResponse();
-        player.GetComponent<PlayerController>().Die();
-        GameController.Instance.NotifyPlayerState(player.GetComponent<PlayerController>(), "player_death", false, null);
-        player.transform.position = GameController.Instance.DeadzonePoint.position;
-        StartCoroutine(ReviveCharacter(player));
-        response.Success = true;
-        return response;
-    }
-
-    private IEnumerator ReviveCharacter(GameObject player)
-    {
-        yield return new WaitForSeconds(2.5f);
-        player.transform.position = GameController.Instance.GetRandomSpawnPoint();
-        player.GetComponent<PlayerController>().ResetShank();
-        player.SetActive(true);
-    }
 }
