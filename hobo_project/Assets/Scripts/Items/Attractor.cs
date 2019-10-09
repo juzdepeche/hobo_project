b5221671c6ec5642307fa979d9c68f1954f6a393
Justin Gagnon-Lavoie <juzdepeche@gmail.com>
2019-10-08 20:11:48 -0400

    Attract objects

---
+++ hobo_project/Assets/Scripts/Items/Attractor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    public float Attraction = 3f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            transform.parent.position = Vector3.MoveTowards(transform.parent.position, other.transform.position, Attraction * Time.deltaTime);
+        }
+    }
+}
